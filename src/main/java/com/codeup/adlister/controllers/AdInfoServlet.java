7e0fdf87a9b944fff9542af9cd216387a60b99f0
Seth Gardner <seth.gardner.a@gmail.com>
2021-09-15 10:03:29 -0500

    got the ad info to display

---
+++ src/main/java/com/codeup/adlister/controllers/AdInfoServlet.java
@@ -0,0 +1,30 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLAdsDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "controllers.AdInfoServlet", urlPatterns = "/ads/ad-info")
+public class AdInfoServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Long id = Long.parseLong(request.getParameter("id"));
+        try {
+            request.setAttribute("ads", DaoFactory.getAdsDao().search(id));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        request.getRequestDispatcher("/WEB-INF/adInfo.jsp").forward(request, response);
+
+    }
+
+
+}
